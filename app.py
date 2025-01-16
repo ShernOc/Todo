7f17d3193e3e0b2e0c8cd9c6196813d779a0fd48
ShernOC <sherlynea1994@hotmail.com>
2025-01-16 16:06:54 +0300

    database migration

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 #import the flask 
 from flask import Flask, jsonify, request 
 from flask_migrate import Migrate
-from models import User,Tag,Todo,db
+from models import User,Tag,Todo, db
 
 #create a flask class 
 app = Flask(__name__)
 
 # #create a migration. config parameters
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 
 # #initialize the router 
-# migrate = Migrate(app,db)
+migrate = Migrate(app,db)
+db.init_app(app)
 
 # #create a router/ where the user will go, 
 
@@ -18,25 +19,23 @@ app = Flask(__name__)
 def index():
     return "<h1>To-Do Application</h1> <p> This is an Todo Application that utilizes the Flask Packages</p> "
 
-@app.route('user/<name>', method = ["GET", "POST"])
-def user(name):
-    return f"<h2>Todos for {name} </h2>"
-pass 
+
+# @app.route('/user/<name>', method = ["GET", "POST"])
+# # def user(name):
+# #     return f"<h2>Todos for {name} </h2>"
+# # pass 
 
 
-@app.route('tag/')
+@app.route('/tag')
 def tag():
     pass
 
 
-@app.route('todo/')
+@app.route('/todo')
 def todo():
     pass 
     
-
-
-
-
+    
 #Run the application: 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- models.py
+++ models.py
@@ -1,13 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_sqlalchemy import MetaData
+from sqlalchemy import MetaData
+
 
 #What do they even mean? 
 metadata = MetaData()
 db = SQLAlchemy(metadata= metadata)
 
-#create the classes of the Todo app. 
-
-#Create the tables: User, Tag, Todo using the Model method
+#Create the tables class : User, Tag, Todo using the Model method
 
 #User table:
 #User: username, email, password, 
@@ -19,10 +18,10 @@ class User(db.Model):
     username = db.Column(db.String(128), nullable = False)
     email = db.Column(db.String(128),nullable =False)
     is_approved = db.Column(db.Boolean, default = False)
-    password = db.Column(db.String(128), nullable = False)
+    password = db.Column(db.String(120), nullable = False)
     
     #create a relationship with the Todo Table
-    todo = db.relationship("Todo",backref="user", lazy = True)
+    todos = db.relationship("Todo",back_populates="Users")
     
     #repr methods returns a string
     def __repr__(self):
@@ -30,30 +29,30 @@ class User(db.Model):
     
 # Tag Table
 class Tag(db.Model):
-    __table__ = "Tag"
+    __tablename__ = "Tags"
     
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(128), nullable=False)
 
     #Create a relationship
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"),nullable = False)
+    todos = db.relationship("Todo", back_populates ="Tags")
     
     def __repr__(self):
         return f"User('{self.name}', '{self.user_id}')"
     
 # To-do table :  
 class Todo(db.Model):
-    __table__ = "Todo"
+    __tablename__ = "Todo"
     
     id = db.Column(db.Integer, primary_key = True)
-    title = db.Colum(db.String(128), nullable = False)
+    title = db.Column(db.String(128), nullable = False)
     description = db.Column(db.String(256))
-    is_complete= db.Colum(db.Boolean, default = False)
+    is_complete= db.Column(db.Boolean, default = False)
     deadline = db.Column(db.String(20), nullable = False)
     
     #create a relationship 
-    user_id = db.Column(db.Integer,db.ForeignKey("user.id"), nullable= False)
-    tag_id = db.Column(db.Integer,db.ForeignKey("tag.id"), nullable= False)
+    user_id = db.Column(db.Integer,db.ForeignKey("Users.id"), nullable= False)
+    tag_id = db.Column(db.Integer,db.ForeignKey("Tags.id"), nullable= False)
     
     def __repr__(self):
         return f"User('{self.title}', '{self.description}', '{self.is_complete}',{self.deadline} ,{self.user_id}, {self.tag_id})"
