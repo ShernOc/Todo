19841ee426f76883322f1b5f20b4e7c27b51cd87
ShernOC <sherlynea1994@hotmail.com>
2025-01-19 20:15:15 +0300

    added the todo/tag routes

--- app.py
+++ app.py
@@ -112,7 +112,6 @@ def update_username(user_id):
     else:
         return jsonify({"error": "User does not exist"}), 406
     
-    
 #DELETE USER; 
 @app.route('/users/<int:user_id>',methods=['DELETE'])
            
@@ -131,6 +130,7 @@ def delete_user(user_id):
 # =========================TAG=================
 
 #Fetch/Get Tags
+#fetch all tags
 @app.route('/tags')
 def get_tags():
     #query.all gets all the tags 
@@ -193,8 +193,23 @@ def update_tag(tag_id):
     
     else:
         return jsonify({"Error": "Name does not exist"}), 406
+    
 
+#fetch one tag 
+@app.route('/tags/<int:id>')
+def fetch_one_tag(id):
+    tagz= Tag.query.get(id)
+    
+    if tagz:
+        return jsonify({
+            "id":tagz.id,
+            "name":tagz.name
+        })
+    else: 
+        return jsonify({"Error":"Tag doesn't exist"})
+    
 #Delete Tag: 
+from datetime import datetime
 @app.route('/tags/<int:tag_id>', methods = ["DELETE"])
 def delete_tag(tag_id):
     #get all the data 
@@ -207,59 +222,122 @@ def delete_tag(tag_id):
     
     else:
         return jsonify({"Error":"Tag does not exist"})
+      
+# =================Todo=====SECTION === 
+#fetch all todos
+@app.route('/todos')
+def get_todo():
+    #get all the todos 
+    todos = Todo.query.all()
+    #create an empty list of tod 
+    todo_list = []
     
+    for todo  in todos:
+        todo_list.append({
+            "id":todo.id,
+            "title":todo.title,
+            "description":todo.description,
+            "is_complete":todo.is_complete,
+            "deadline":todo.deadline,
+            "user_id":todo.user_id,
+            "tag_id": todo.tag_id
+        }
+        )
     
-#=================TODO==============
+    return jsonify({"Todo-List ": todo_list})
 
+#fetch one Todo based on id 
+@app.route('/tags/<int:id>')
+def fetch_one_todo(id):
+    todo= Tag.query.get(id)
     
-# def update_tags(name)
-#     #check if user exists by query.get( which is a default using the get)
-#     tags = Tag.query.get(d)
+    if todo:
+        return jsonify({
+            "id":todo.id,
+            "title":todo.title,
+            "description":todo.description,
+            "is_complete":todo.is_complete,
+            "deadline":todo.deadline,
+            "user_id":todo.user_id,
+            "tag_id": todo.tag_id,
+        })
+    else: 
+        return jsonify({"Error":"Tag doesn't exist"})
     
-#     if user: # if user exist
-#         #get the data 
-#         data = request.get_json()
-#         username = data['username']
-#         email = data['email']
-#         password = data['password']
-        
-#         #get username and the id is not equal to the username we are searching. 
-#         #Check if the username or the id is not equal to the id. 
-#         check_username = User.query.filter_by(username=username and id!=user.id).first()
-#         check_email = User.query.filter_by(email = email and id!=user.id).first()
-        
-#         if check_username or check_email:
-#             return jsonify({"error": "Username/Email already exist"}), 406
-        
-#         #and if not, 
-#         else: 
-#             #user is the user that was fetched user = User.query.get(user_id)
-#             user.username = username
-#             user.email = email
-#             user.password = password 
-        
-#         #Just commit no adding. 
-#             db.session.commit()
-#             return jsonify({"Success": "Users updated successfully"}), 201
-# #if the user does not exist? 
-#     else:
-#         return jsonify({"error": "User does not exist"}), 406
+#Add a todo list 
+@app.route('/todos', methods= ['POST'])
+def add_todo():
+    #initialize the data 
+    data = request.get_json()
+    title = data['title']
+    description=data['description']
+    user_id=data['user_id']
+    tag_id= data['tag_id']
+    try:
+        deadline = datetime.strptime(data['deadline'], "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        return jsonify({"error":"invalid datetime"})
+    
+    #check if they exist: 
+    check_user_id = User.query.get(user_id)
+    check_tag_id = Tag.query.get(tag_id)
+    
+    #if they dont exist, then 
+    if not check_user_id or not check_tag_id:
+        return jsonify({"Error": "Todo does not exist"}), 406
+    
+    else: 
+        new_todo = Todo(title = title, description = description, user_id = user_id, tag_id = tag_id, deadline = deadline)
+        db.session.add(new_todo)
+        db.session.commit()
+        return jsonify({"Success":"Todo added successfully" })
     
     
-# #DELETE USER; 
-# @app.route('/users/<int:user_id>',methods=['DELETE'])
+#Update a todo 
+@app.route('/todos/<todo_id>')
+def update_todos(todo_id):
+    #check if todo exists by query.get( which is a default using the get)
+    todo= Tag.query.get(todo_id)
+    
+    if todo: # if user exist
+        #get the data 
+        data = request.get_json()
+        title = data['title']
+        description = data['description']
+        tag_id = data['tag_id']
+        user_id = data['user_id']
+        try:
+            deadline = datetime.strptime(data['deadline'], "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            return jsonify({"error":"invalid datetime"})
+    
+        check_title = Todo.query.filter_by(title = title).first()
+        check_description = Todo.query.filter_by(description = description).first()
+
+        if check_title or check_description:
+            return jsonify({"error": "title/description already exist"})
+        else:
+            todo.title = title
+            todo.description = description
+            todo.tag_id = tag_id
+            todo.deadline = deadline
+            todo.user_id = user_id
+        
+        #calling the function
+            db.session.commit()
+            return jsonify({"success": "todo updated successfully"})
+    else:
+        return jsonify({"error":"todo doesn't exist"})
            
-# def delete_user(user_id):
-#     #get the users
-#     user = User.query.get(user_id)
-    
-#     if user:
-#         db.session.delete(user)
-#         db.session.commit()
-#         return jsonify({"Sucess":"User Deleted Successfully"})
+#DELETE Todo; 
+@app.route('/todos/<int:todo_id>', methods =['DELETE'])
+def delete_todo(todo_id):
+    #get the users
+    todos = Todo.query.get(todo_id)
+    if todos:
+        db.session.delete(todos)
+        db.session.commit()
+        return jsonify({"Success Todo, successfully deleted"})
 
-#     else:
-#          return jsonify({"Error": "User does not exist"})
-
-
-
+    else:
+         return jsonify({"Error": "Todo does not exist, Try again"})
