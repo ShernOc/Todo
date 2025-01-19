0b7d12a9b883e27b6380e11309103fc8197a96b6
ShernOC <sherlynea1994@hotmail.com>
2025-01-19 14:33:56 +0300

    adding users

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 migrate = Migrate(app,db)
 db.init_app(app)
 
-# #create a router/ where the user will go, 
+#create a router/ where the user will go, 
+# Fetch Users/Get Users 
 
 @app.route('/')
 def index():
@@ -25,34 +26,45 @@ def index():
 # #     return f"<h2>Todos for {name} </h2>"
 # # pass 
 
+#Fetch/Get Users 
+@app.route('/users')
+def get_users():
+    return jsonify({["events A", "event B"]})
 
-@app.route('/tag')
-def tag():
-    pass
-
-
-@app.route('/todo')
-def todo():
-    pass 
+#Add a Users/ Add you use the method POST 
+#1. Receive the data in json: 
+#To add ? you have to check if the user first exists, if yes you create an error message that the user exist, if not you add the user 
+#2. You initialize: data = request.get_json()
+@app.route('/users', methods=["POST"])
+def add_users():
+    data = request.get_json() # This is an object in json 
+    username = data['username']
+    email = data['email']
+    password = data['password']
     
+#3. Check if the users exists
+    check_username = User.query.filter_by(username=username).first() # first selects the first username in order. 
+    check_email= User.query.filter_by(email=email).first()
+
+#4. create an error message if the username or email exists, if yes: return an error message 200/ is just an indication of the error
+    #prints the output 
+    print("Username", check_username)
+    print("Email", check_email)
     
-#Run the application: 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
-    
-# #To add 
-# user = User()
-# db.session.add(user)
-# db.session.commit()
+    if check_username or check_email: 
+        return jsonify({"error":"username/email already exist"}),406
+    else:  # This is where we now add the new username
+        new_user = User(username = username, email = email, password = password )
+#5. Next you now update/by calling the db.session.add(new_user) db.session.commit()
+
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"Success": "Users added successfully"})
 
 
-# #To update/modify data 
-# user.verified = True
-# db.session.commit()
-
-# #to delete
-# db.session.delete(user)
-# db.session.commit()
 
 
-    
+
+
+#Connect to the POSTMAN
+#1. Copy the Url http://127.0.0.1:5000
